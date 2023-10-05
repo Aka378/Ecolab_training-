821a9a82f96dca8855c747e9895469440fc18c51
Aka378 <91967241+Aka378@users.noreply.github.com>
2023-10-05 11:00:41 +0530

    Update Books.py

--- assignment_9.1/Books.py
+++ assignment_9.1/Books.py
@@ -5,92 +5,92 @@ class Book:
         self.author = author
         self.price = price
         self.rating = rating
+        self.previous = None  # Previous book in the linked list
+        self.next = None  # Next book in the linked list
 
 class Bookstore:
     def __init__(self):
-        self.books = []
+        self.head = None  # Initialize the linked list with an empty head node
 
     def add_book(self, book):
-        self.books.append(book)
+        new_book = Book(book.id, book.title, book.author, book.price, book.rating)
+        if not self.head:
+            self.head = new_book
+        else:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_book
+            new_book.previous = curr  # Set the previous book for the new book
 
     def find_by_id(self, book_id):
-        for book in self.books:
-            if book.id == book_id:
-                return book
+        curr = self.head
+        while curr:
+            if curr.id == book_id:
+                return curr
+            curr = curr.next
         return None
 
     def find_by_author(self, author_name):
         books_by_author = []
-        for book in self.books:
-            if book.author == author_name:
-                books_by_author.append(book)
+        curr = self.head
+        while curr:
+            if curr.author == author_name:
+                books_by_author.append(curr)
+            curr = curr.next
         return books_by_author
 
     def find_by_rating_range(self, min_rating, max_rating):
         books_in_range = []
-        for book in self.books:
-            if min_rating <= book.rating <= max_rating:
-                books_in_range.append(book)
+        curr = self.head
+        while curr:
+            if min_rating <= curr.rating <= max_rating:
+                books_in_range.append(curr)
+            curr = curr.next
         return books_in_range
 
     def find_by_price_range(self, min_price, max_price):
         books_in_range = []
-        for book in self.books:
-            if min_price <= book.price <= max_price:
-                books_in_range.append(book)
+        curr = self.head
+        while curr:
+            if min_price <= curr.price <= max_price:
+                books_in_range.append(curr)
+            curr = curr.next
         return books_in_range
-
 
 # create some books
 book1 = Book(1, "book1", "author1", 2000, 4.5)
 book2 = Book(2, "book2", "author2", 1500, 4.2)
 
-
 # create a bookstore to add books
 bookstore = Bookstore()
 bookstore.add_book(book1)
 bookstore.add_book(book2)
-bookstore.add_book(book3)
-while True:
-    print("\nMenu:")
-    print("1. Find book by ID")
-    print("2. Find books by author")
-    print("3. Find books by rating range")
-    print("4. Find books by price range")
-    print("5. Exit")
-    
-    choice = input("Enter your choice: ")
 
-    if choice == "1":
-        book_id = int(input("Enter book ID: "))
-        found_book = bookstore.find_by_id(book_id)
-        if found_book:
-            print(f"Title: {found_book.title}, Author: {found_book.author}")
-        else:
-            print("Book not found.")
-    elif choice == "2":
-        author_name = input("Enter author's name: ")
-        books_by_author = bookstore.find_by_author(author_name)
-        print("\nBooks found by author:")
-        for book in books_by_author:
-            print(f"Title: {book.title}, Author: {book.author}")
-    elif choice == "3":
-        min_rating = float(input("Enter minimum rating: "))
-        max_rating = float(input("Enter maximum rating: "))
-        books_by_rating = bookstore.find_by_rating_range(min_rating, max_rating)
-        print("\nBooks found by rating range:")
-        for book in books_by_rating:
-            print(f"Title: {book.title}, Rating: {book.rating}")
-    elif choice == "4":
-        min_price = float(input("Enter minimum price: "))
-        max_price = float(input("Enter maximum price: "))
-        books_by_price = bookstore.find_by_price_range(min_price, max_price)
-        print("\nBooks found by price range:")
-        for book in books_by_price:
-            print(f"Title: {book.title}, Price: ${book.price}")
-    elif choice == "5":
-        print("Exiting the search menu")
-        break
-    else:
-        print("Invalid choice.")
+# Example usage of Bookstore class methods
+book_id = 1
+found_book = bookstore.find_by_id(book_id)
+if found_book:
+    print(f"Title: {found_book.title}, Author: {found_book.author}")
+else:
+    print("Book not found.")
 
+author_name = "author1"
+books_by_author = bookstore.find_by_author(author_name)
+print("\nBooks found by author:")
+for book in books_by_author:
+    print(f"Title: {book.title}, Author: {book.author}")
+
+min_rating = 4.0
+max_rating = 4.5
+books_by_rating = bookstore.find_by_rating_range(min_rating, max_rating)
+print("\nBooks found by rating range:")
+for book in books_by_rating:
+    print(f"Title: {book.title}, Rating: {book.rating}")
+
+min_price = 1000
+max_price = 2000
+books_by_price = bookstore.find_by_price_range(min_price, max_price)
+print("\nBooks found by price range:")
+for book in books_by_price:
+    print(f"Title: {book.title}, Price: ${book.price}")
