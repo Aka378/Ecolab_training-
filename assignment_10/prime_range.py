909e7d0ee5c35d8e46933b4f542b40fe8056c427
Aka378 <91967241+Aka378@users.noreply.github.com>
2023-10-08 12:54:52 +0530

    Update prime_range.py

--- assignment_10/prime_range.py
+++ assignment_10/prime_range.py
@@ -10,7 +10,7 @@ class PrimeRange:
     def is_prime(self, num):
         if num <= 1:
             return False
-        for i in range(2, int(num**0.5) + 1):
+        for i in range(2, num):
             if num % i == 0:
                 return False
         return True
